5eaaab18ba475a9c3047f0ea6303ee748f92f641
Juan001234 <142929273+Juan001234@users.noreply.github.com>
2023-08-22 13:29:17 -0500

    Add files via upload

---
+++ helloword/SextoPrograma.py
@@ -0,0 +1,12 @@
+#2 elevado a la décima potencia, or 1024
+print(2 ** 10)
+ 
+# 8 al cuadrado, or 64
+print(8 ** 2)
+ 
+# 9 * 9 * 9, 9 al cubo, or 729
+print(9 ** 3)
+ 
+# Incluso podemos realizar exponentes fraccionarios
+# 4 a la mitad de la potencia, o 2
+print(4 ** 0.5)
