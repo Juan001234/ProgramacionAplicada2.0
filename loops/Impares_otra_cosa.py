a01022a5c74ed53431673e1d3d9b85690e2dc67b
Juan001234 <142929273+Juan001234@users.noreply.github.com>
2023-09-05 13:07:39 -0500

    Add files via upload

---
+++ loops/Impares_otra_cosa.py
@@ -0,0 +1,14 @@
+ 
+times = input("Digite numero de impresiones: ")
+times = int(times)
+for i in range (1,times+1):
+     residual = i%2
+     if residual == 0:
+         print(f'{i} is even')
+     else:
+         #print(f'{i} is odd')
+         print(str(i) + ' is odd')
+
+for i in range (0,times):
+    result = i**3
+    print(result)
