81487deda276b5770d6d6c1f1cd6546806038d57
Juan001234 <142929273+Juan001234@users.noreply.github.com>
2023-10-01 18:58:19 -0500

    Update archivoCSV.py

--- Corte2/archivosCSV/archivoCSV.py
+++ Corte2/archivosCSV/archivoCSV.py
@@ -1,27 +1,28 @@
 import csv
 import random
  
-# field names
 Titulos = ['Nombre', 'Numero_Celular', 'Edad', 'Codigo_Estudiante']
- 
-# data rows of csv file
+ # Nombre principal de cada columna
+
 Columnas = [ ['Juan Jose', '3165642875', '20', '202120'],
         ['Miguel', '3102314675', '18', '201956'],
         ['Daniel', '3209517538', '25', '202145'],
         ['Estiven', '3174628731', '16', '202045'],
         ['Fernando', '3217412589', '27', '201946'],
         ['Andres', '3143698521', '20', '201945']]
+# Datos de cada columna
  
-# name of csv file
 filename = "Datos.csv"
- 
-# writing to csv file
+ # Nobre del archivo CSV
+
+# Escribiendo archivo CSV
+
 with open(filename, 'w') as csvfile:
-    # creating a csv writer object
+    # Creando archivo CSV
     csvwriter = csv.writer(csvfile)
      
-    # writing the fields
+    # Escribiendo titulos de las columnas
     csvwriter.writerow(Titulos)
      
-    # writing the data rows
+    # Escribiendo datos de las columnas
     csvwriter.writerows(Columnas)
