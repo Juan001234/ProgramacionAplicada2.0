d96a3e266935ba4adcbdc279a2066dd41ac42967
Juan001234 <142929273+Juan001234@users.noreply.github.com>
2023-11-16 09:10:04 -0500

    Update libreriaNumpy.py

--- Corte2/numpy/libreriaNumpy.py
+++ Corte2/numpy/libreriaNumpy.py
@@ -1,1 +1,21 @@
 #Introdiccion a numpy
+import numpy
+
+arr = numpy.array([1, 2, 3, 4, 5])
+
+print(arr) #arreglo simple con libreria numpy
+
+#Importar numpy con apodo
+import numpy as np
+
+arr = np.array([1, 2, 3, 4, 5])
+
+print(arr)
+
+#verificar version de numpy
+import numpy as np
+
+print(np.__version__)
+
+
+#
